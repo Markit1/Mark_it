9589cacb356a3cc10ea843c95b3713cf55ad90bc
EderVs <eedervs.97@hotmail.com>
2015-03-20 18:28:40 -0600

    Terminado el demo

--- Markit_page/scripts/popup.js
+++ Markit_page/scripts/popup.js
@@ -1,29 +1,25 @@
-$(document).ready(function(){
-  $( "input[id=markit_page], a, button" )
+/*jslint unparam: true*/
+var chrome;
+$(document).ready(function () {
+  $("input[id=markit_page], a, button")
     .button()
-    .click(function( event ) {
-     chrome.tabs.executeScript( null, {code:"alert('hola Markit');"}
-     //,function(results){ alert("hola"); }
-     );
-
+    .click(function (event) {
+      chrome.tabs.executeScript(null, {code: "alert('hola Markit');"});
     });
 
-    var content = $('#txt_name').val();
-    $(function() {
-
-    $('#txt_name').keyup(function() {
-        if ($('#txt_name').val() != content) {
-            content = $('#txt_name').val();
-        }
+  var content = $('#txt_name').val();
+  $(function () {
+    $('#txt_name').keyup(function () {
+      if ($('#txt_name').val() !== content) {
+        content = $('#txt_name').val();
+      }
     });
-    });
-
-    $( "input[id=share], a, button" )
-      .button()
-      .click(function( event ) {
-        //alert(content);
-       chrome.tabs.executeScript( null, {code:"location.href = 'http://www.google.com'"}
-       ,function(results){}
-       );
-      });
   });
+
+  $("input[id=share], a, button")
+    .button()
+    .click(function (event) {
+      //alert(content);
+      chrome.tabs.executeScript(null, {code: "location.href = 'https://mark-it.herokuapp.com/'"});
+    });
+});
